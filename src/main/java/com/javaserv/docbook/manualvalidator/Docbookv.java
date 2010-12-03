dc8d8e65ba5cb6988041c0852bfa015533787096
DanielBruessler <mail@danielbruessler.de>
2010-12-03 17:22:24 +0100

    update

--- src/main/java/com/javaserv/docbook/manualvalidator/Docbookv.java
+++ src/main/java/com/javaserv/docbook/manualvalidator/Docbookv.java
@@ -6,7 +6,7 @@ package com.javaserv.docbook.manualvalidator;
  * @since 0.1.0
  */
 public class Docbookv {
-	static public String VERSION = "0.2.4";
+	static public String VERSION = "0.2.8";
 	static public String VERSION_DESCRIPTION = "Result in JSON-format with parameter --json, dokumentation refactoring and extended for parameters help und --json";
 
 	public static void main(String[] args){
