1192d0300cda6b2c83f0ce09a67023e60f6eb52d
DanielBruessler <mail@danielbruessler.de>
2010-12-03 13:04:58 +0100

    M 0.2 "Filename as parameter and optional the type against it shall be validated"

--- docbook.sh
+++ docbook.sh
@@ -1,2 +1,3 @@
 #!/bin/bash
-java -jar docbookv.jar
+#$0 is needed for the helptext
+java -jar docbookv.jar $0 $1 $2
--- src/main/java/com/javaserv/docbook/manualvalidator/Docbookv.java
+++ src/main/java/com/javaserv/docbook/manualvalidator/Docbookv.java
@@ -3,6 +3,6 @@ package com.javaserv.docbook.manualvalidator;
 public class Docbookv {
 
 	public static void main(String[] args){
-		System.out.println("OK");
+		VerboseInfo userInfo = new VerboseInfo(args);
 	}
 }
